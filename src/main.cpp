6fd879448a6d26b829f0d0a6fb2f77021028ac48
marquinho94 <marcoalanmiranda@gmail.com>
2023-03-27 17:51:32 -0300

    sume los .pio y una funcion

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+int variable_prueba
 
+
+int funcionParaQuePreguntes (void);
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
+
+ variable_prueba = funncionParaQuePreguntes();
   // put your main code here, to run repeatedly:
+}
+
+int funcionParaQuePreguntes (void)
+{
+  int a=1;
+  return a;
+
 }
